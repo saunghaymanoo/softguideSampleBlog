d2b0859a39174142409f6f67039930f3285943db
saunghaymanoo <saunghaymanoo322@gmail.com>
2022-08-28 16:12:12 +0900

    changes

--- 5_sample_blog/template/header.php
+++ 5_sample_blog/template/header.php
@@ -47,7 +47,7 @@
                                 <a class="dropdown-item" href="#">Action</a>
                                 <a class="dropdown-item" href="#">Another action</a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#">Logout</a>
+                                <a class="dropdown-item" href="<?php echo $url; ?>/logout.php">Logout</a>
                             </div>
                         </div>
                     </div>
